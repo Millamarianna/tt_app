b46028349c147998acfc33c8fbe51c80d13822a6
Millamarianna <milla.tuomainen@outlook.com>
2023-12-05 10:38:27 +0200

    Azurea varten kokeilu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 import Home from './screens/Home';
 import AdminHome from './screens/AdminHome';
 import AdminCustomers from './screens/AdminCustomers';
 import Layout from "./components/Layout";
 
 function App() {
-  
-  return (<BrowserRouter>
-  <Routes>
-<Route path="/" element={<Layout content={<Home />} />} />
-<Route path="/admin" element={<Layout content={<AdminHome />} />} />
-<Route path="/customers" element={<Layout content={<AdminCustomers />} />} />
-  </Routes>
-</BrowserRouter>
-    
-  );
+  return (
+    <div>
+      <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/admin" component={AdminHome} />
+          <Route path="/customers" component={AdminCustomers} />
+      </div>
+      <div>Hi</div>
+      </div>
+  ); 
 }
+
 
 export default App;
