2a4e0f42f57302c2fa70fbda7722751cc2ad1c79
Millamarianna <milla.tuomainen@outlook.com>
2023-12-05 12:40:36 +0200

    removed router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { HashRouter, Route, Link } from "react-router-dom";
-import Home from './screens/Home';
-import AdminHome from './screens/AdminHome';
-import AdminCustomers from './screens/AdminCustomers';
-import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div>
-      <div>
-          <Route exact path="/" component={Home} />
-          <Route path="/admin" component={AdminHome} />
-          <Route path="/customers" component={AdminCustomers} />
-      </div>
+    
       <div>Hi</div>
-      </div>
+     
   ); 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
      <App />
-     </HashRouter>
   </React.StrictMode>
 );
 
