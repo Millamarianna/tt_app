e9519d18602719cc79f3561fbd047c30573b2cad
Millamarianna <milla.tuomainen@outlook.com>
2023-12-07 10:17:14 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
+const root = document.getElementById('root');
+
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
      <App />
