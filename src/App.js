561ba54bdba17d58ee50330732591551d2803e7c
Millamarianna <milla.tuomainen@outlook.com>
2023-12-07 10:53:01 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
 
 function App() {
   return (
     
-      <div>Hi</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route index element={<Home />} />
+        <Route path="blogs" element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
      
   ); 
 }
