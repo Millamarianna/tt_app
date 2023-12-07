61e9fbdd205de67ce004649bf3bac4542614f5b8
Millamarianna <milla.tuomainen@outlook.com>
2023-12-07 11:13:26 +0200

    changed the workflow file to add CI false

--- src/screens/AdminCustomers.js
+++ src/screens/AdminCustomers.js
@@ -1,4 +1,4 @@
-import { Text, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const AdminCustomers = () => {
   const [fetchAgain, setFetchAgain] = useState(0);
