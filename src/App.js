bb8f1fd918f86eaa9dfcaed6bf1ef98f4fbb9019
Millamarianna <milla.tuomainen@outlook.com>
2023-12-07 11:04:00 +0200

    added customerfetch from backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
+import AdminCustomers from "./screens/AdminCustomers";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}>
         <Route index element={<Home />} />
-        <Route path="blogs" element={<Home />} />
+        <Route path="customers" element={<AdminCustomers />} />
       </Route>
     </Routes>
   </BrowserRouter>
